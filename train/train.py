371decaf39195412ca497ad5d47bba5690d0a4c5
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-04-15 19:28:54 -0400

    first commit

--- train/train.py
+++ train/train.py
@@ -184,7 +184,7 @@ def evaluate_captioning(
         print(sample[0].shape)
         break
 
-    for batch_n, batch in enumerate(tqdm(test_dataloader, desc=f"Running inference {dataset_name.upper()}")):
+    for batch_n, batch in enumerate(tqdm(data['train'].dataloader, desc=f"Running inference {dataset_name.upper()}")):
         print(len(batch[0]))
 
         batch_images = []
