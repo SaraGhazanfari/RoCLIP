7dedfae5077f22570b9bf3da9f1015d969df3ca3
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-04-16 16:46:50 -0400

    first commit

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -89,8 +89,8 @@ class LlavaMetaForCausalLM(ABC):
 
     def encode_images(self, images):
         image_features = self.get_model().get_vision_tower()(images)
-        print(images.device, image_features.device)
-        print(self.get_model().mm_projector.device)
+        print(self.get_model().device)
+        print(self.get_model().mm_projector(image_features))
         image_features = self.get_model().mm_projector(image_features)
         return image_features
 
