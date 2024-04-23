028e0c3dcf0a4e0b999bd82e95d50e67db14eeda
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-04-23 16:19:48 -0400

    add new dataset for question answering

--- open_flamingo/eval/models/llava.py
+++ open_flamingo/eval/models/llava.py
@@ -1,19 +1,16 @@
 import copy
 import os
-
 from typing import List
 
 import torch
-
 from torchvision.transforms import transforms
 
-from open_flamingo.eval.eval_model import BaseEvalModel
+from llava.constants import DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
+from llava.conversation import conv_templates, SeparatorStyle
+from llava.mm_utils import tokenizer_image_token, process_images, get_model_name_from_path
 from llava.model.builder import load_pretrained_model
 from llava.utils import disable_torch_init
-
-from llava.mm_utils import tokenizer_image_token, process_images, get_model_name_from_path
-from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
-from llava.conversation import conv_templates, SeparatorStyle
+from open_flamingo.eval.eval_model import BaseEvalModel
 
 
 class EvalModelLLAVA(BaseEvalModel):
@@ -31,13 +28,14 @@ class EvalModelLLAVA(BaseEvalModel):
         model_path = os.path.expanduser(model_args["model_path"])
         model_name = get_model_name_from_path(model_path)
         self.model, self.image_processor, self.tokenizer, context_len = load_pretrained_model(
-            model_path, model_args.get("model_base"), model_name, pretrained_rob_path=model_args["vision_encoder_pretrained"],
+            model_path, model_args.get("model_base"), model_name,
+            pretrained_rob_path=model_args["vision_encoder_pretrained"],
             dtype=model_args["precision"]
-            )
+        )
         self.image_processor.do_normalize = False
         self.normalizer = transforms.Normalize(
             mean=self.image_processor.image_mean, std=self.image_processor.image_std
-            )  # we need to normalize in the forward pass, so that the threat model is consistent
+        )  # we need to normalize in the forward pass, so that the threat model is consistent
         model_args["temperature"] = float(model_args["temperature"])
         model_args["num_beams"] = int(model_args["num_beams"])
         self.model_args = model_args
@@ -51,22 +49,24 @@ class EvalModelLLAVA(BaseEvalModel):
 
         self.dataset_name = model_args.get("dataset_name")
 
-        self.stop_str = conv_templates[self.conv_mode].sep if conv_templates[self.conv_mode].sep_style != SeparatorStyle.TWO else conv_templates[self.conv_mode].sep2
+        self.stop_str = conv_templates[self.conv_mode].sep if conv_templates[
+                                                                  self.conv_mode].sep_style != SeparatorStyle.TWO else \
+        conv_templates[self.conv_mode].sep2
         self.stop_token_id = self.tokenizer.convert_tokens_to_ids(self.stop_str)
 
     @torch.no_grad()
     def get_outputs(
-        self,
-        batch_text,  # List[conv object]
-        batch_images: torch.Tensor,
-        min_generation_length: int,
-        max_generation_length: int,
-        **kwargs,
+            self,
+            batch_text,  # List[conv object]
+            batch_images: torch.Tensor,
+            min_generation_length: int,
+            max_generation_length: int,
+            **kwargs,
     ) -> List[str]:
         assert len(batch_text) == 1, "Only support batch size 1 (yet)"
         assert 0. <= batch_images.min() and batch_images.max() <= 1., "Images must be in image space"
 
-        #prompt = batch_text.get_prompt()
+        # prompt = batch_text.get_prompt()
         input_ids = self._prepare_text(batch_text)
 
         batch_images = self.normalizer(batch_images)
@@ -113,10 +113,10 @@ class EvalModelLLAVA(BaseEvalModel):
         return out.loss.unsqueeze(0)
 
     def set_inputs(
-        self,
-        batch_text,
-        past_key_values: torch.Tensor = None,
-        to_device: bool = False,
+            self,
+            batch_text,
+            past_key_values: torch.Tensor = None,
+            to_device: bool = False,
     ):
         self.input_ids = self._prepare_text(batch_text)
 
@@ -131,7 +131,6 @@ class EvalModelLLAVA(BaseEvalModel):
         self.labels = labels
         self.attention_mask = self.input_ids.ne(self.tokenizer.pad_token_id)
         self.past_key_values = past_key_values
-
 
     def _prepare_images(self, batch: List[List[torch.Tensor]]) -> torch.Tensor:
         assert len(batch) == 1, "Only support batch size 1 (yet)"
@@ -148,8 +147,10 @@ class EvalModelLLAVA(BaseEvalModel):
     def get_vqa_prompt(self, question, answer=None) -> str:
         if self.dataset_name == "vizwiz":
             self.prompt_suffix = "\nWhen the provided information is insufficient, respond with 'Unanswerable'.\nAnswer the question using a single word or phrase."
-        elif self.dataset_name in ["textvqa", "vqav2", "cc3m"]:
+        elif self.dataset_name in ["textvqa", "vqav2"]:
             self.prompt_suffix = "\nAnswer the question using a single word or phrase."
+        elif self.dataset_name == 'cc3m':
+            self.prompt_suffix = ""
         else:
             raise ValueError(f"Unknown dataset: {self.dataset_name}")
             self.prompt_suffix = ""
