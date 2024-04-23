43b29c12a968c10b8936d284a9abb40802929064
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-04-23 11:22:22 -0400

    add new dataset for question answering

--- vlm_eval/captioning/vqa_eval.py
+++ vlm_eval/captioning/vqa_eval.py
@@ -235,6 +235,7 @@ def evaluate_vqa(
 
             batch_images = eval_model._prepare_images(batch_images)
             print(batch_images.shape)
+            print(batch_images[0].shape)
             if args.from_saved:
                 assert args.batch_size == 1
                 assert init == "clean", "not implemented"
@@ -278,7 +279,7 @@ def evaluate_vqa(
                 # save the adversarial images
                 q_id = batch["question_id"][i]
                 adv_images_cur_dict[q_id] = batch_images[i]
-            print(batch_images.shape)
+
             outputs = eval_model.get_outputs(
                 batch_images=batch_images,
                 batch_text=batch_text,
