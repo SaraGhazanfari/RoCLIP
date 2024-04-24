a4154eb5d0591afba92ba71e3727c49bcbec0e25
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-04-24 14:03:23 -0400

    add new dataset for question answering

--- finetune/sup_adv_train_clip.py
+++ finetune/sup_adv_train_clip.py
@@ -185,7 +185,6 @@ def main(args, leftovers):
     model = EvalModelLLAVA(
         dict(
             vision_encoder_pretrained=model_args['vision_encoder_pretrained'],
-            precision='float32',
             **model_args,
         ),
     )
