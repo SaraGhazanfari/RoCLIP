b75f39f335b4c8f9ad1d15a2bca84d2cdcbd284c
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-05-02 09:33:38 -0400

    add intermediate save_model

--- llava/model/language_model/llava_llama.py
+++ llava/model/language_model/llava_llama.py
@@ -69,9 +69,7 @@ class LlavaLlamaForCausalLM(LlamaForCausalLM, LlavaMetaForCausalLM):
             output_hidden_states if output_hidden_states is not None else self.config.output_hidden_states
         )
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-        print('++++++++++')
-        print(images.device)
-        print(self.model.device)
+
         input_ids, attention_mask, past_key_values, inputs_embeds, labels = self.prepare_inputs_labels_for_multimodal(
             input_ids, attention_mask, past_key_values, labels, images)
 
