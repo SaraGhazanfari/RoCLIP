c52444cad70b40a8dfd6e8e4c84715ca3bcfa37d
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-05-09 11:48:34 -0400

    add intermediate save_model

--- llava/model/llava_arch.py
+++ llava/model/llava_arch.py
@@ -97,6 +97,10 @@ class LlavaMetaForCausalLM(ABC):
             images, image_sizes=None):
         vision_tower = self.get_vision_tower()
         if vision_tower is None or images is None or input_ids.shape[1] == 1:
+            if past_key_values is not None and vision_tower is not None and images is not None and input_ids.shape[
+                1] == 1:
+                attention_mask = torch.ones((attention_mask.shape[0], past_key_values[-1][-1].shape[-2] + 1),
+                                            dtype=attention_mask.dtype, device=attention_mask.device)
             return input_ids, attention_mask, past_key_values, None, labels
 
         if type(images) is list or images.ndim == 5:
