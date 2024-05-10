01cc2471d409081524a694c72da9b15d5b45eabb
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-05-10 11:47:45 -0400

    add textvqa to evaluation

--- vlm_eval/captioning/vqa_eval.py
+++ vlm_eval/captioning/vqa_eval.py
@@ -231,7 +231,8 @@ def evaluate_vqa(
                 images=eval_model.normalizer(
                     batch_images.to(dtype=eval_model.model.dtype, device='cuda', non_blocking=True)),
             )
-
+            print(torch.argmax(complete_outputs['logits'][0], dim=1))
+            print(torch.argmax(complete_outputs['logits'][0], dim=1).shape)
             output_ids = torch.argmax(complete_outputs['logits'][0], dim=1).reshape(-1, 1)
 
             input_token_len = eval_model.input_ids.shape[1]
