d1296c098e7aca6aac3f1971fe8e7c83a6973e67
saraghazanfari <s.ghazanfari.ce@gmail.com>
2024-05-13 16:15:03 -0400

    add noisy image

--- rs/smooth.py
+++ rs/smooth.py
@@ -103,7 +103,7 @@ class Smooth(object):
                                                                               max_generation_length=self.max_generation_length,
                                                                               num_beams=self.num_beams,
                                                                               length_penalty=self.length_penalty)
-                scores = scores[0, :]
+                scores = scores[0:1, :]
                 counts += self._count_arr(scores.argmax(1).cpu().numpy(), self.num_classes)
             return counts
 
